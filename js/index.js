6e12cf7fa137a18e55f28afb55e7ee8c3eb99cfa
Juan <juanfassa1993@gmail.com>
2022-11-16 23:22:48 -0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,22 @@
+//let inicio;
+//inicio = 0;
+
+//let nombre = "Juan"
+//let suma = 2+2
+
+//let ingreseDatos = prompt("Ingrese su nombre")
+
+//alert ("Bienvenido + ingreseDatos");
+
+//let texto = prompt ("Ingrese una palabra")
+//texto === "Hola" ? alert("Has dicho hola") : alert("Has dicho otra cosa")
+
+//let cincoelementos= ["gato","perro","loro","delfin","vaca"]
+    //console.log(cincoelementos.length)
+
+// function saludar(){
+//     alert("Hola Mundo!");
+// }
+// saludar();
+
+
